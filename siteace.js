7e0443bbb96c2cb136e64599b804cb883fd27c45
Fahimul Aleem <fahimul.aleem@gmail.com>
2016-07-17 00:10:43 +0600

    Changed Startup

--- siteace.js
+++ siteace.js
@@ -160,33 +160,6 @@ if (Meteor.isServer) {
     // code to run on server at startup
     if (!Websites.findOne()){
     	console.log("No websites yet. Creating starter data.");
-    	  Websites.insert({
-    		title:"Goldsmiths Computing Department",
-    		url:"http://www.gold.ac.uk/computing/",
-    		description:"This is where this course was developed.",
-    		createdOn:new Date(),
-				createdBy: "anonymous",
-				upVote:0,
-				downVote:0
-    	});
-    	 Websites.insert({
-    		title:"University of London",
-    		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
-    		description:"University of London International Programme.",
-    		createdOn:new Date(),
-				createdBy: "anonymous",
-				upVote:0,
-				downVote:0
-    	});
-    	 Websites.insert({
-    		title:"Coursera",
-    		url:"http://www.coursera.org",
-    		description:"Universal access to the world’s best education.",
-    		createdOn:new Date(),
-				createdBy: "anonymous",
-				upVote:0,
-				downVote:0
-    	});
     	Websites.insert({
     		title:"Google",
     		url:"http://www.google.com",
